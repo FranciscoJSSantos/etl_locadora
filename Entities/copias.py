3c1476f024a0753d41b8356aadf48fbb5f7e499b
Francisco José <francisco@infowsoftware.com.br>
2022-04-05 19:50:37 -0300

    criacao das entidades

---
+++ Entities/copias.py
@@ -0,0 +1,6 @@
+class Copias():
+  def __init__(self, cod_tit, num_cop, data_aq, status) -> None:
+    self.cod_tit = cod_tit
+    self.num_cop = num_cop
+    self.data_aq = data_aq
+    self.status = status
